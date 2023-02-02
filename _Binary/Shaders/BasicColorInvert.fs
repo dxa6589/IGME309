2e1b559f5d24298a9c80de99d59d28f6136bbd8f
Dolapo <dxa6589@rit.edu>
2023-02-01 19:08:30 -0500

    Exercise 03 submission

--- _Binary/Shaders/BasicColorInvert.fs
+++ _Binary/Shaders/BasicColorInvert.fs
@@ -9,18 +9,18 @@ out vec4 Fragment;
 
 void main()
 {
-	vec3 output = Color;
+	vec3 final = Color;
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 	{
-		output = SolidColor;
+		final = SolidColor;
 	}
 	
 	if(Complimentary == true)
 	{
-		output = vec3(1.0f, 1.0f, 1.0f) - output;
+		final = vec3(1.0f, 1.0f, 1.0f) - final;
 	}
 	
-	Fragment = vec4(output,1);
+	Fragment = vec4(final,1);
 		
 	return;
 }
